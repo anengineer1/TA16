e6f086297ca58cfccf0c3a0a1bddf9082e5ccb29
Francisco Parra <franparpe@disroot.org>
2023-03-27 21:36:45 +0200

    Finalizar ejercicios

--- Ejercicio_1.sql
+++ Ejercicio_1.sql
@@ -1,82 +1,97 @@
 USE `actividades`;
 
--- # Mostrar el nombre de los artículos
+SELECT * FROM articulos;
+SELECT * FROM fabricantes;
 
+-- # Mostrar el nombre de los artículos
+-- Ejercicio 1
 SELECT NOMBRE FROM articulos;
 
 -- # Nombres y precios
-
+-- Ejercicio 2
 SELECT NOMBRE,PRECIO FROM articulos;
 
 -- # Nombres articuls <= 200
-
+-- Ejercicio 3
 SELECT NOMBRE FROM articulos WHERE precio <= 200;
 
 -- # Nombres precio comprendido entre 60 y 120
-
+-- Ejercicio 4
 SELECT NOMBRE FROM articulos WHERE precio BETWEEN 60 AND 120;
 
 -- # Nombre y precio en pesetas
-
+-- Ejercicio 5
 SELECT NOMBRE, PRECIO*166.386 AS PRECIO_PESETAS FROM articulos;
 
 -- # Precio medio de cada fabricante
-
+-- Ejercicio 6
 SELECT fabricantes.NOMBRE, AVG(articulos.PRECIO) FROM
 (articulos INNER JOIN fabricantes ON articulos.FABRICANTE = fabricantes.CODIGO)
 GROUP BY NOMBRE;
 
 -- Precio medio mayor cuyo número de fabricante sea 2
-
+-- Ejercicio 7
 SELECT fabricantes.NOMBRE, AVG(articulos.PRECIO) FROM
 (articulos INNER JOIN fabricantes ON articulos.FABRICANTE = fabricantes.CODIGO) WHERE fabricantes.CODIGO=2 GROUP BY NOMBRE;
 
 -- Número de articulos mayor a 180
+-- Ejercicio 8
 SELECT COUNT(PRECIO) AS NUM_ARTICULOS_MAYOR_A_180 FROM articulos WHERE PRECIO > 180;
 
 -- Ordenar descendentemente por precio si mayor a 180 y desc nombre
+-- Ejercicio 9
 SELECT NOMBRE,PRECIO FROM articulos WHERE PRECIO > 180 ORDER BY PRECIO DESC, NOMBRE ASC;
 
 -- Lista completa artículos con su fabricante
+-- Ejercicio 10
 SELECT articulos.NOMBRE, fabricantes.NOMBRE FROM articulos
 INNER JOIN fabricantes
 ON articulos.FABRICANTE=fabricantes.CODIGO;
 
 -- List con precios también
+-- Ejercicio 11
 SELECT articulos.NOMBRE, articulos.PRECIO, fabricantes.NOMBRE FROM articulos
 INNER JOIN fabricantes
 ON articulos.FABRICANTE=fabricantes.CODIGO;
 
 -- Lista media por código fabricante
+-- Ejercicio 12
 SELECT fabricantes.CODIGO, AVG(articulos.PRECIO) FROM
 (articulos INNER JOIN fabricantes ON articulos.FABRICANTE = fabricantes.CODIGO) GROUP BY CODIGO;
 
 -- Lista media por nombre fabricante
+-- Ejercicio 13
 SELECT fabricantes.NOMBRE, AVG(articulos.PRECIO) FROM
 (articulos INNER JOIN fabricantes ON articulos.FABRICANTE = fabricantes.CODIGO) GROUP BY NOMBRE;
 
 -- Lista valor medio >= 150
+-- Ejercicio 14
 SELECT fabricantes.NOMBRE FROM
 (articulos INNER JOIN fabricantes ON articulos.FABRICANTE = fabricantes.CODIGO)
 GROUP BY fabricantes.NOMBRE HAVING AVG(articulos.PRECIO) >= 150;
 
 -- Articulos más barato
+-- Ejercicio 15
 SELECT NOMBRE, PRECIO FROM articulos ORDER BY PRECIO LIMIT 1;
 
 -- Articulo más caro de cada fabricante
-SELECT articulos.NOMBRE, fabricantes.NOMBRE, MAX(articulos.PRECIO) FROM
-(articulos INNER JOIN fabricantes ON articulos.FABRICANTE = fabricantes.CODIGO)
-GROUP BY fabricantes.NOMBRE, articulos.NOMBRE;
--- Revisar
+-- Ejercicio 16
+SELECT articulos.NOMBRE, fabricantes.NOMBRE, articulos.PRECIO FROM articulos
+JOIN fabricantes ON fabricantes.CODIGO=articulos.FABRICANTE
+WHERE articulos.PRECIO IN (SELECT MAX(articulos.PRECIO) FROM articulos GROUP BY FABRICANTE);
 
--- Añadir nuevo producto
+-- Añadir nuevo product
+-- Ejercicio 17
 INSERT INTO articulos VALUES (11, 'Altavoces', 70, 2);
 
 -- Cambiar nombre producto 8
+-- Ejercicio 18
 UPDATE articulos SET NOMBRE='Impresora Laser' WHERE CODIGO=8;
 
 -- Descuento del 10%
+-- Ejercicio 19
 UPDATE articulos SET PRECIO=PRECIO*0.9;
 
 -- Descuento de 10€
+-- Ejercicio 20
 UPDATE articulos SET PRECIO=(PRECIO-10) WHERE PRECIO >= 120;
--- Ejercicio_2.sql
+++ Ejercicio_2.sql
@@ -1,59 +1,79 @@
 USE `actividades`;
 
+-- Ejercicio 1
 SELECT APELLIDOS FROM empleados;
 
+-- Ejercicio 2
 SELECT APELLIDOS FROM empleados GROUP BY APELLIDOS;
 
+-- Ejercicio 3
 SELECT * FROM empleados WHERE APELLIDOS='Smith';
 
+-- Ejercicio 4
 SELECT * FROM empleados WHERE APELLIDOS='Smith' OR APELLIDOS='Rogers';
 
+-- Ejercicio 5
 SELECT * FROM empleados WHERE DEPARTAMENTO=14;
 
+-- Ejercicio 6
 SELECT * FROM empleados WHERE DEPARTAMENTO=37 OR DEPARTAMENTO=77;
 
+-- Ejercicio 7
 SELECT * FROM empleados WHERE APELLIDOS LIKE 'P%';
 
+-- Ejercicio 8
 SELECT SUM(PRESUPUESTO) AS PRESUPUESTO_TOTAL FROM departamentos;
 
+-- Ejercicio 9
 SELECT COUNT(DNI) AS NUMERO_DE_EMPLEADOS, DEPARTAMENTO FROM empleados GROUP BY DEPARTAMENTO;
 
+-- Ejercicio 10
 SELECT empleados.DNI,empleados.NOMBRE,empleados.APELLIDOS,departamentos.NOMBRE FROM empleados
 INNER JOIN departamentos ON empleados.DEPARTAMENTO=departamentos.CODIGO;
 
+-- Ejercicio 11
 SELECT empleados.DNI,empleados.NOMBRE,empleados.APELLIDOS,
 departamentos.NOMBRE,departamentos.PRESUPUESTO
 FROM empleados
 INNER JOIN departamentos ON empleados.DEPARTAMENTO=departamentos.CODIGO;
 
+-- Ejercicio 12
 SELECT empleados.NOMBRE,empleados.APELLIDOS
 FROM empleados
 INNER JOIN departamentos ON empleados.DEPARTAMENTO=departamentos.CODIGO
 WHERE departamentos.PRESUPUESTO > 60000;
 
 -- Datos de los deparamentos con media por encima
+-- Ejercicio 13
 SELECT * FROM departamentos WHERE PRESUPUESTO > (SELECT AVG(PRESUPUESTO) FROM departamentos);
 
 -- Nombres con departamentos de más de 2 empleados
+-- Ejercicio 14
 SELECT NOMBRE FROM departamentos WHERE CODIGO IN (SELECT DEPARTAMENTO FROM
 empleados GROUP BY DEPARTAMENTO HAVING COUNT(DEPARTAMENTO) > 2);
 
 -- Añadir calidad
+-- Ejercicio 15
 INSERT INTO departamentos VALUES (11, 'Calidad', 40000);
 -- Añadir Ester
 INSERT INTO empleados VALUES ('89267109', 'Esther', 'Vázquez', 11);
 
 -- Aplicar recorte presupuesto del 10% (multiplicar por 0.9)
+-- Ejercicio 16
 UPDATE departamentos SET PRESUPUESTO=PRESUPUESTO*0.9;
 
 -- Reasignar empleados
+-- Ejercicio 17
 UPDATE empleados SET DEPARTAMENTO=14 WHERE DEPARTAMENTO=77;
 
 -- Despedir a todos los del 14
+-- Ejercicio 18
 DELETE FROM empleados WHERE DEPARTAMENTO=14;
 
 -- Despedir por encima a los 60000
+-- Ejercicio 19
 DELETE FROM empleados WHERE DEPARTAMENTO IN (SELECT CODIGO FROM departamentos WHERE PRESUPUESTO > 60000);
 
 -- Despedir a todos
+-- Ejercicio 20
 TRUNCATE empleados;
